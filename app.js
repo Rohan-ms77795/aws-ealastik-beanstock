1a585ef2104cfb805d7172345ce9f5dd2888cfc7
Rohan sawant <131663263+Rohan-ms77795@users.noreply.github.com>
2024-05-16 14:22:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('welcome to Devops - Rohan sawant!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
